d1618156ec6a3715c576a6d7e481043fdb981856
Nedyalko Kikov <inktech.n.kikov@gmail.com>
2016-07-09 21:56:05 +0300

    create user and password

--- mySQL.php
+++ mySQL.php
@@ -1,9 +1,17 @@
 <?php
 $mysqli = new mysqli("localhost", "root", "", "blog");
-$users = $mysqli->query("SELECT * FROM users");
-while($user = $users->fetch_assoc()){
-	$username = $user['username'];
-	echo htmlspecialchars($username) . "<br>\n";
-}
+//$users = $mysqli->query("SELECT * FROM users");
+//while($user = $users->fetch_assoc()){
+//	$username = $user['username'];
+//	echo htmlspecialchars($username) . "<br>\n";
 
+$statement = $mysqli->prepare("INSERT INTO users(username, password_hash, full_name)" . "VALUES (?,?,?)");
+$username = 'pesho2';
+$pass = crypt('password123');
+$fullname = 'Petio';
+$statement->bind_param("sss",$username,$pass,$fullname);
+$statement->execute();
+if($statement->affected_rows==1){
+	echo "succes";
+}
 ?>
